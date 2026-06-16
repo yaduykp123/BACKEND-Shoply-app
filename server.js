0d686accaf548afa70b0efbd2bde893129c1ef76
Yadu Krishnan <yaduykp123@gmail.com>
2026-06-16 16:14:09 +0530

    changed .env file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./src/config/db');
 
 connectDB();
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
