ad92c1f1a0995385683ce4f3708cd1f676740cad
Yadu Krishnan <yaduykp123@gmail.com>
2026-06-18 09:53:13 +0530

    changed app.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 const connectDB = require('./src/config/db');
 
-connectDB(); // db.js exports connectMDB, aliased here as connectDB
+connectDB(); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
